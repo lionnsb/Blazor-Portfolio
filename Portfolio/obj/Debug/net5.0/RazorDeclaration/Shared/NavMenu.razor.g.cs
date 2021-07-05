1550b5f0316b24bea093660b5e7ce67b865d2198
Seyer <Seyer@HAA-N18007>
2021-07-05 20:55:24 +0200

    Checkin 2

--- Portfolio/obj/Debug/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
+++ Portfolio/obj/Debug/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
@@ -13,70 +13,70 @@ namespace Portfolio.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\Seyer\source\repos\Portfolio\Portfolio\_Imports.razor"
+#line 1 "C:\Users\Seyer\Documents\git\Blazor\Blazor-Portfolio\Portfolio\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Seyer\source\repos\Portfolio\Portfolio\_Imports.razor"
+#line 2 "C:\Users\Seyer\Documents\git\Blazor\Blazor-Portfolio\Portfolio\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Seyer\source\repos\Portfolio\Portfolio\_Imports.razor"
+#line 3 "C:\Users\Seyer\Documents\git\Blazor\Blazor-Portfolio\Portfolio\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Seyer\source\repos\Portfolio\Portfolio\_Imports.razor"
+#line 4 "C:\Users\Seyer\Documents\git\Blazor\Blazor-Portfolio\Portfolio\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Seyer\source\repos\Portfolio\Portfolio\_Imports.razor"
+#line 5 "C:\Users\Seyer\Documents\git\Blazor\Blazor-Portfolio\Portfolio\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Seyer\source\repos\Portfolio\Portfolio\_Imports.razor"
+#line 6 "C:\Users\Seyer\Documents\git\Blazor\Blazor-Portfolio\Portfolio\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Seyer\source\repos\Portfolio\Portfolio\_Imports.razor"
+#line 7 "C:\Users\Seyer\Documents\git\Blazor\Blazor-Portfolio\Portfolio\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Seyer\source\repos\Portfolio\Portfolio\_Imports.razor"
+#line 8 "C:\Users\Seyer\Documents\git\Blazor\Blazor-Portfolio\Portfolio\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Seyer\source\repos\Portfolio\Portfolio\_Imports.razor"
+#line 9 "C:\Users\Seyer\Documents\git\Blazor\Blazor-Portfolio\Portfolio\_Imports.razor"
 using Portfolio;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\Seyer\source\repos\Portfolio\Portfolio\_Imports.razor"
+#line 10 "C:\Users\Seyer\Documents\git\Blazor\Blazor-Portfolio\Portfolio\_Imports.razor"
 using Portfolio.Shared;
 
 #line default
@@ -90,7 +90,7 @@ using Portfolio.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 28 "C:\Users\Seyer\source\repos\Portfolio\Portfolio\Shared\NavMenu.razor"
+#line 28 "C:\Users\Seyer\Documents\git\Blazor\Blazor-Portfolio\Portfolio\Shared\NavMenu.razor"
        
     private bool collapseNavMenu = true;
 
